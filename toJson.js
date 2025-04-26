f20383820beb02448046a7eb1309e93e142f1c27
yyanoo <78666961+yyanoo@users.noreply.github.com>
2025-04-26 10:51:37 +0900

    Update toJson.js

--- toJson.js
+++ toJson.js
@@ -11,30 +11,62 @@ async function scrapeCardInfo(n) {
         const { data: html } = await axios.get(url);
         const $ = load(html);
 
-        const cardId = $('dt:contains("カード番号")').next('dd').find('p').html();
-        if(!cardId){
+        const cardId = pdd('dt:contains("カード番号")');
+        if (!cardId) {
+            console.error(`Error at card OS01/R01-${n}:`, e.message);
             return null;
         }
+
+        function pdd(target) {
+            return $(target)
+                .next('dd')
+                .find('p')
+                .html();
+        }
+
+
+        function img(target) {
+            return $(target)
+                .next('dd')
+                .find('img')
+                .map((i, el) => $(el).attr('alt'))
+                .get();
+        }
+        
+        //卡名
+        const cardName = $('.item-Heading')
+            .contents()
+            .not('span')
+            .text()
+            .trim()
+
+        //魂刻
+        const soul = String($('dt:contains("ソウル")')
+            .next('dd')
+            .find('img')
+            .length);
 
         //效果分段
         const effect = $('dt:contains("テキスト")').next('dd').find('p').html();
         const effectList = effect.replace(/<br\s*\/?>/gi, '\n').trim();
         const [effect1, effect2, effect3] = effectList.split('\n');
 
-        return {
+        return{
             "卡號": cardId,
-            "卡名": $('.item-Heading').contents().not('span').text().trim(),
+            "卡名": cardName,
             "圖": `https://ws-rose.com/wordpress/wp-content/images/cardlist/os${title}/r${title}/os${title}_r${title}_${n}.png`,
-            "色": $('img.color').attr('alt'),
-            "等級": $('dt:contains("レベル")').next('dd').find('p').html(),
-            "費用": $('dt:contains("コスト")').next('dd').find('p').html(),
-            "攻擊力": $('dt:contains("パワー")').next('dd').find('p').html(),
+            "色": img('dt:contains("色")'),
+            "判定": img('dt:contains("トリガー")'),
+            "等級": pdd('dt:contains("レベル")'),
+            "費用": pdd('dt:contains("コスト")'),
+            "攻擊力": pdd('dt:contains("パワー")'),
+            "魂": soul,
             "效果1": effect1,
             "效果2": effect2,
             "效果3": effect3
-        };
+        }
     } catch (e) {
-        console.error(`Error at card ${n}:`, e.message);
+        console.error(`Error at card OS01/R01-${n}:`, e.message);
         return null;
     }
 }
