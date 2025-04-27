86da4aff5596c9536a3fcecc544223e3ebb50b61
yyanoo <78666961+yyanoo@users.noreply.github.com>
2025-04-28 01:52:56 +0900

    Update reptilesToJson_wsr.js

--- reptilesToJson_wsr.js
+++ reptilesToJson_wsr.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 import { load } from 'cheerio';
 import { writeFile } from 'fs/promises';
 
-const title = ''; //系列號 只需要 title OS{title}/R{title}-{n}
+const title = '01'; //系列號 只需要 title OS{title}/R{title}-{n}
 
-//抓資料
+
+//抓補充包
 async function scrapeCardInfo(n) {
     try {
         const url = `https://ws-rose.com/cardlist/?cardno=OS${title}/R${title}-${n}`;
@@ -14,7 +15,6 @@ async function scrapeCardInfo(n) {
         //判斷是否有該卡號
         const cardId = pdd('dt:contains("カード番号")');
         if (!cardId) {
-            console.error(`Error at card OS01/R01-${n}:`, e.message);
             return null;
         }
 
@@ -34,7 +34,7 @@ async function scrapeCardInfo(n) {
                 .map((i, el) => $(el).attr('alt'))
                 .get();
         }
-        
+
         //卡名 去除span的片假名
         const cardName = $('.item-Heading')
             .contents()
@@ -54,10 +54,10 @@ async function scrapeCardInfo(n) {
         const [effect1, effect2, effect3] = effectList.split('\n');
 
         //回傳資料 到def
-        return{
+        return {
             "卡號": cardId,
             "卡名": cardName,
-            "圖": `https://ws-rose.com/wordpress/wp-content/images/cardlist/os${title}/r${title}/os${title}_r${title}_${n}.png`,
+            "圖": `https://ws-rose.com${$('.thumbnail-Inner img').attr('src')}`,
             "色": img('dt:contains("色")'),
             "判定": img('dt:contains("トリガー")'),
             "等級": pdd('dt:contains("レベル")'),
@@ -69,19 +69,168 @@ async function scrapeCardInfo(n) {
             "效果3": effect3
         }
     } catch (e) {
-        console.error(`Error at card OS01/R01-${n}:`, e.message);
+        return null;
+    }
+}
+
+// 抓預組
+async function scrapeCardTdInfo(n) {
+    try {
+        const url = `https://ws-rose.com/cardlist/?cardno=OS${title}/R${title}-T${n}`;
+        const { data: html } = await axios.get(url);
+        const $ = load(html);
+
+        //判斷是否有該卡號
+        const cardId = pdd('dt:contains("カード番号")');
+        if (!cardId) {
+            return null;
+        }
+
+        //dd 裏的 p
+        function pdd(target) {
+            return $(target)
+                .next('dd')
+                .find('p')
+                .html();
+        }
+
+        //顔色+魂刻 能抓兩個以上alt
+        function img(target) {
+            return $(target)
+                .next('dd')
+                .find('img')
+                .map((i, el) => $(el).attr('alt'))
+                .get();
+        }
+
+        //卡名 去除span的片假名
+        const cardName = $('.item-Heading')
+            .contents()
+            .not('span')
+            .text()
+            .trim()
+
+        //魂刻
+        const soul = String($('dt:contains("ソウル")')
+            .next('dd')
+            .find('img')
+            .length);
+
+        //效果br來換行 并且分割成效果1,2,3
+        const effect = $('dt:contains("テキスト")').next('dd').find('p').html();
+        const effectList = effect.replace(/<br\s*\/?>/gi, '\n').trim();
+        const [effect1, effect2, effect3] = effectList.split('\n');
+
+        //回傳資料 到def
+        return {
+            "卡號": cardId,
+            "卡名": cardName,
+            "圖": `https://ws-rose.com${$('.thumbnail-Inner img').attr('src')}`,
+            "色": img('dt:contains("色")'),
+            "判定": img('dt:contains("トリガー")'),
+            "等級": pdd('dt:contains("レベル")'),
+            "費用": pdd('dt:contains("コスト")'),
+            "攻擊力": pdd('dt:contains("パワー")'),
+            "魂": soul,
+            "效果1": effect1,
+            "效果2": effect2,
+            "效果3": effect3
+        }
+    } catch (e) {
+        return null;
+    }
+}
+
+//抓PR
+async function scrapeCardPrInfo(n) {
+    try {
+        const url = `https://ws-rose.com/cardlist/?cardno=OS${title}/R${title}-P${n}`;
+        const { data: html } = await axios.get(url);
+        const $ = load(html);
+
+        //判斷是否有該卡號
+        const cardId = pdd('dt:contains("カード番号")');
+        if (!cardId) {
+            return null;
+        }
+
+        //dd 裏的 p
+        function pdd(target) {
+            return $(target)
+                .next('dd')
+                .find('p')
+                .html();
+        }
+
+        //顔色+魂刻 能抓兩個以上alt
+        function img(target) {
+            return $(target)
+                .next('dd')
+                .find('img')
+                .map((i, el) => $(el).attr('alt'))
+                .get();
+        }
+
+        //卡名 去除span的片假名
+        const cardName = $('.item-Heading')
+            .contents()
+            .not('span')
+            .text()
+            .trim()
+
+        //魂刻
+        const soul = String($('dt:contains("ソウル")')
+            .next('dd')
+            .find('img')
+            .length);
+
+        //效果br來換行 并且分割成效果1,2,3
+        const effect = $('dt:contains("テキスト")').next('dd').find('p').html();
+        const effectList = effect.replace(/<br\s*\/?>/gi, '\n').trim();
+        const [effect1, effect2, effect3] = effectList.split('\n');
+
+        //回傳資料 到def
+        return {
+            "卡號": cardId,
+            "卡名": cardName,
+            "圖": `https://ws-rose.com${$('.thumbnail-Inner img').attr('src')}`,
+            "色": img('dt:contains("色")'),
+            "判定": img('dt:contains("トリガー")'),
+            "等級": pdd('dt:contains("レベル")'),
+            "費用": pdd('dt:contains("コスト")'),
+            "攻擊力": pdd('dt:contains("パワー")'),
+            "魂": soul,
+            "效果1": effect1,
+            "效果2": effect2,
+            "效果3": effect3
+        }
+    } catch (e) {
         return null;
     }
 }
 
 //資料存cards轉Json 更改系列到上 title
-
 async function scrapeAllCards() {
     const cards = [];
-
+    console.log('start');
+    
     for (let i = 1; ; i++) {
         const padded = String(i).padStart(3, '0');
         const card = await scrapeCardInfo(padded);
+        if (!card) break;
+        cards.push(card);
+    }
+
+    for (let i = 1; ; i++) {
+        const padded = String(i).padStart(2, '0');
+        const card = await scrapeCardTdInfo(padded);
+        if (!card) break;
+        cards.push(card);
+    }
+
+    for (let i = 1; ; i++) {
+        const padded = String(i).padStart(2, '0');
+        const card = await scrapeCardPrInfo(padded);
         if (!card) break;
         cards.push(card);
     }
